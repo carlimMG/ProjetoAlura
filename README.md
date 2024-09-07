01e0ac197de696250f547b18b6d50c3707a4cc85
carlimMG <hch181359@gmail.com>
2024-09-07 19:08:25 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+Esse projeto é so para mostrar algumas coisas sobre o ciclismo
+	-modalidades
+	-texto explicando cada modalidade com foto para mostrar qual bicicleta usada em cada esporte
+	-rank de 3 atletas masculino e feminino do rank brasileiro e mundial, com um pequeno texto sobre ele(a) é tambem um link e cada atleta caso a pessoa queira ver mais informações sobre o atleta e a foto para a pessoa vizulizar sobre quem está lendo.
+	-é uma simples barra de pesquisa para a pessoa pesquisar sobre alguma atleta(mais tem poucos atletas na lista por motivos de tempo)
+	
+
+!!! barra de pesquisar funciona apenas com os atletas que coloquei na lista, dexei a lista no menu para fornecer o nome de alguns atletas.
+
+tela 21.45 Polegadas
+	
---
+++ dados.js
@@ -0,0 +1,106 @@
+let dados = [
+    {
+        titulo: "Henrique Avancini",
+        descricao: "Começou a competir aos 8 anos de idade, com uma bicicleta de sucatas montada da oficina do seu pai, Ruy Avancini, reaproveitou um quadro de bicicleta quebrado de tamanho muito maior (22'') do que o tamanho usado por Henrique e foi transformado num quadro tamanho 12,5 especialmente para ele. Henrique da Silva Avancini cresceu em Petrópolis com sua família, sob uma formação cristão-evangélica. Avancini rapidamente encontrou seu equilíbrio e, crescendo no campo, foi capaz de habilmente aprimorar suas habilidades MTB cross country. Com trabalho duro e determinação, o brasileiro se tornou o primeiro piloto em seu país a vencer o campeonato nacional em todas as categorias, desde Infanto Juvenil até a Elite. Agora próspero entre a elite no estágio do mundo, Avancini continua a desenvolver suas habilidades e está olhando sempre para melhorar seus resultados.",
+        link: "https://www.google.com/search?q=Henrique+Avancini&rlz=1C1VDKB_pt-PTBR1087BR1087&oq=Henrique+Avancini&gs_lcrp=EgZjaHJvbWUyDggAEEUYJxg5GIAEGIoFMgoIARAuGLEDGIAEMgcIAhAAGIAEMgcIAxAAGIAEMgcIBBAAGIAEMgcIBRAAGIAEMgcIBhAAGIAEMgYIBxBFGDzSAQgxODMwajBqOagCALACAA&sourceid=chrome&ie=UTF-8",
+        tags: ""
+    },
+    {
+        titulo: "Mauro Ribeiro",
+        descricao: "Mauro Ribeiro (Curitiba, 19 de julho de 1964) é um ex-ciclista profissional e técnico brasileiro, atualmente aposentado e dono de sua própria marca de roupas.Incentivado por Renato Romeu, no ano de 1980, aos 16 anos de idade, Mauro iniciou o ciclismo profissional na equipe Cicles Clube Romeu.Em 1981, com 17 anos de idade já era campeão pan-americano adulto e júnior.No ano seguinte foi campeão mundial e, em 1983, conquistou a medalha de bronze nos 400 metros por equipe nos Jogos Pan-Americanos de Caracas.Seu principal triunfo esportivo foi a vitória na 7° etapa do Paris-Nice em 1990, no percurso de 102 km de Mandelieu-la-Napoule-Nice, e a conquista da 9ª etapa do Tour de France 1991 (entre Alençon e Rennes), realizada no dia 14 de julho (data comemorativa da queda da Bastilha e feriado nacional). Foi o primeiro brasileiro com esta situação no Tour de France.",
+        link: "https://pt.wikipedia.org/wiki/Mauro_Ribeiro",
+        tags: ""
+    },
+    {
+        titulo: "Ana Vitória Gouvea Vieira Almeida Magalhães",
+        descricao: "Ana Vitória Gouvea Vieira Almeida Magalhães é uma atleta emergente no mundo do ciclismo, destacando-se por seu talento e dedicação ao esporte. Nascida em um ambiente que valoriza o esporte e a atividade física, Ana Vitória desenvolveu desde cedo uma paixão pelo ciclismo. Sua trajetória no ciclismo começou com competições locais, onde rapidamente se destacou por sua habilidade e determinação. Com o tempo, ela conseguiu se destacar em competições regionais e nacionais, conquistando títulos importantes e ganhando reconhecimento na comunidade do ciclismo. Sua dedicação ao treinamento e sua capacidade de enfrentar desafios a ajudaram a estabelecer uma carreira promissora. Ana Vitória é conhecida não apenas por suas conquistas nas competições, mas também por seu papel como inspiração para jovens ciclistas, especialmente mulheres, mostrando que com esforço e comprometimento, é possível alcançar grandes objetivos no esporte. Ela continua a trabalhar duro para alcançar novos marcos em sua carreira e contribui para o crescimento do ciclismo, promovendo o esporte e engajando-se em iniciativas que visam desenvolver novos talentos e fomentar o interesse pelo ciclismo.",
+        link: "https://www.google.com.br/search?q=Ana+Vit%C3%B3ria+Gouvea+Vieira+Almeida+Magalh%C3%A3es&sca_esv=cfc38f4587fd6d45&hl=pt-BR&biw=619&bih=952&sxsrf=ADLYWIK3M149d_hoRun0gIhINfG2RAp0uA%3A1725654417884&ei=kWXbZrbUNY7I1sQP-dmksA4&ved=0ahUKEwj24NTUk6-IAxUOpJUCHfksCeYQ4dUDCBA&uact=5&oq=Ana+Vit%C3%B3ria+Gouvea+Vieira+Almeida+Magalh%C3%A3es&gs_lp=Egxnd3Mtd2l6LXNlcnAiLUFuYSBWaXTDs3JpYSBHb3V2ZWEgVmllaXJhIEFsbWVpZGEgTWFnYWxow6NlczIGEAAYFhgeMggQABiABBiiBDIIEAAYgAQYogQyCBAAGIAEGKIEMggQABiABBiiBEjSClDDBFjDBHACeAGQAQCYAZsBoAGbAaoBAzAuMbgBA8gBAPgBAfgBApgCA6ACwwGoAhLCAgcQIxgnGOoCwgIHEC4YJxjqAsICFhAuGIAEGEMYtAIYyAMYigUY6gLYAQGYAxm6BgYIARABGAiSBwMyLjGgB-UD&sclient=gws-wiz-serp",
+        tags: ""
+    },
+    {
+        titulo: "Raiza Goulão",
+        descricao: "Raiza Goulão é uma ciclista profissional brasileira, conhecida por suas performances destacadas no ciclismo de montanha. Nascida em 1988, ela se especializou em competições de mountain bike (MTB) e cross-country. Goulão ganhou notoriedade por seu talento e habilidade técnica nas trilhas, acumulando vários títulos e prêmios ao longo de sua carreira. Além de suas conquistas em competições nacionais e internacionais, Raiza também é uma figura importante na promoção do ciclismo feminino no Brasil. Seu trabalho e dedicação têm inspirado muitas jovens ciclistas a seguir seus passos e buscar a excelência no esporte.",
+        link: "https://www.google.com/search?q=Raiza+Goul%C3%A3o&rlz=1C1VDKB_pt-PTBR1087BR1087&oq=Raiza+Goul%C3%A3o&gs_lcrp=EgZjaHJvbWUyDggAEEUYJxg5GIAEGIoFMgYIARBFGDsyBwgCEC4YgAQyCAgDEAAYFhgeMgoIBBAAGIAEGKIEMgoIBRAAGIAEGKIEMgoIBhAAGIAEGKIEMgYIBxBFGDzSAQgxMTE3ajBqOagCALACAQ&sourceid=chrome&ie=UTF-8",
+        tags: ""
+    },
+    
+    {
+        titulo: "Vinícius Rangel",
+        descricao: "Vinícius Rangel é um ciclista brasileiro que tem se destacado no cenário internacional. Aos poucos, ele vem conquistando seu espaço no pelotão mundial e se tornando uma das principais esperanças do ciclismo brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Vin%C3%ADcius_Rangel",
+        tags: ""
+    },
+
+        
+    {
+        titulo: "Ulan Galinski",
+        descricao: "É um nome que tem ganhado cada vez mais destaque no cenário do mountain bike brasileiro. Com sua habilidade técnica e paixão pelo esporte, ele se tornou uma das maiores promessas do ciclismo nacional.",
+        link: "https://olympics.com/pt/noticias/ulan-galinski-paris-2024",
+        tags: ""
+    },
+        
+    {
+        titulo: "Mathieu van der Poel",
+        descricao: "é, sem dúvida, um dos maiores ciclistas da atualidade. Sua versatilidade e domínio em diversas modalidades do ciclismo o transformaram em um verdadeiro fenômeno.Campeão Mundial: Venceu o Campeonato Mundial de Estrada em 2023.Clássicas Monumentais: Conquistou diversas vezes a Volta à Flandres e a Paris-Roubaix, duas das mais importantes clássicas do ciclismo. Ciclocross: Dominou a modalidade por vários anos, conquistando diversos títulos mundiais. ",
+        link: "https://pt.wikipedia.org/wiki/Mathieu_van_der_Poel",
+        tags: ""
+    },
+        
+    {
+        titulo: "Tadej Pogacar",
+        descricao: "Pogacar começou sua jornada no ciclismo ainda muito jovem, em sua Eslovênia natal. Rapidamente se destacou por seu talento natural e paixão pelo esporte. Sua ascensão meteórica o levou a conquistar títulos importantes nas categorias de base, chamando a atenção de equipes profissionais.consagração definitiva veio em 2020, quando, com uma performance espetacular, conquistou o Tour de France, tornando-se um dos mais jovens vencedores da história da Grande Boucle. No ano seguinte, repetiu o feito, demonstrando uma consistência impressionante para um ciclista tão jovem. ",
+        link: "https://pt.wikipedia.org/wiki/Tadej_Poga%C4%8Dar",
+        tags: ""
+    },
+        
+    {
+        titulo: "Alejandro Valverde",
+        descricao: "Alejandro Valverde Belmonte (Múrcia, 25 de abril de 1980) é um ciclista profissional espanhol que participa em competições de ciclismo de estrada. É considerado por muitos, o ciclista mais completo da actualidade. Conta com inúmeras vitórias, sendo de destacar os triunfos no Campeonato do Mundo de 2018, na Vuelta a España, Fléche Wallone e na Liège-Bastogne-Liège.",
+        link: "https://pt.wikipedia.org/wiki/Alejandro_Valverde",
+        tags: ""
+    },
+        
+    {
+        titulo: "Paôla Reis",
+        descricao: "A ciclista começou a praticar BMX aos 11 anos, através do Projeto Pedal, que é executado pela Associação de Bicicross de Salvador (ABS), em parceria com Governo do Estado, por meio da Superintendência dos Desportos do Estado da Bahia (Sudesb), autarquia da Secretaria do Trabalho, Emprego e Esporte (Setre), em Stella Maris. Nos primeiros anos, disputou torneios nacionais com o apoio da ABS mais doações de amigos e comerciantes do bairro. No entanto, desde os 16 anos, há oito anos atrás, Paola conta com o apoio da Sudesb que custeia as passagens para disputar competições fora do estado e já recebeu bolsa do programa FazAtleta e Bolsa Esporte.",
+        link: "https://olympics.com/pt/noticias/paola-reis-unica-representante-brasil-bmx-racing-paris-2024",
+        tags: ""
+    },
+        
+    {
+        titulo: "Ana Vitória Magalhães",
+        descricao: "É uma ciclista brasileira de estrada e pista , que atualmente compete pela equipe continental feminina da UCI Bepink–Bongioanni. Ela foi selecionada para competir na corrida de estrada feminina nas Olimpíadas de Verão de 2024. Ela também competiu no Giro d'Italia Feminino em 2023 e 2024 e é duas vezes campeã nacional de nível elite, com um título cada na corrida de estrada e no contra-relógio.3.º no Campeonato do Brasil Contrarrelógio Campeonato do Brasil em Estrada",
+        link: "https://en.wikipedia.org/wiki/Ana_Vit%C3%B3ria_Magalh%C3%A3es",
+        tags: ""
+    },
+    {
+        titulo: "Raiza Goulão",
+        descricao: "Raiza Goulão Henrique (Pirenópolis, 28 de fevereiro de 1991) é uma ciclista olímpica brasileira especializada no mountain bike. Representou o Brasil nos Jogos Olímpicos de Verão de 2016 no cross-country feminino do ciclismo, e nos Jogos Olímpicos de Verão de 2024 em Paris. Raiza se tornou bicampeã pan-americana sub-23 ao vencer as edições de Pueblo em 2012 e Tucumán em 2013. No adulto, conquistou a medalha de prata nos Jogos Sul-Americanos de 2014 em Santiago. Representou o Brasil também nos Jogos Pan-Americanos de 2015, evento em que ela terminou em 5.º lugar no cross-country.",
+        link: "https://blog.bikeregistrada.com.br/raiza-goulao/",
+        tags: ""
+    },
+    {
+        titulo: "Anna van der Breggen",
+        descricao: "Anna van der Breggen (Zwolle, 18 de abril de 1990) é uma ciclista profissional neerlandesa. Estreia como profissional em 2009 e depois de requalificar-se amador durante 2 anos definitivamente em 2011 consolidou-se no profissionalismo. Apesar disso, anteriormente já tinha disputado corridas importantes como o Giro d'Italia Feminino com a selecção dos Países Baixos mas com discretos resultados. e a Fleche Wallonne três vezes: em 2015, 2016 e 2017. No mesmo ano, ela foi a campeã no Campeonato dos Países Baixos de Ciclismo em Estrada e terminou em primeiro lugar na temporada mundial. Em 2016, ela tornou-se campeã olímpica nos Jogos Olímpicos de Verão de 2016.",
+        link: "https://pt.wikipedia.org/wiki/Anna_van_der_Breggen",
+        tags: ""
+    },
+    {
+        titulo: "Lizzie Armitstead",
+        descricao: "É uma desportista britânica que compete no ciclismo na modalidade de estrada, pertencendo à equipa Boels Dolmans desde o ano 2013; ainda que também disputou carreiras de pista, onde foi especialista nas provas de perseguição por equipas, pontuação e scratch. É a campeã mundial de estrada do ano 2015 e campeã mundial em pista do ano 2009 em perseguição por equipas. Participou em dois Jogos Olímpicos de Verão, obtendo em Londres 2012 uma medalha de prata na prova de rota feminina e o 10.º lugar na contrarrelógio, e em Rio de Janeiro 2016 o 5.º lugar em estrada. Em estrada obteve três medalhas no Campeonato Mundial de Ciclismo em Estrada, nos anos 2015 e 2016.",
+        link: "https://pt.wikipedia.org/wiki/Lizzie_Armitstead",
+        tags: ""
+    },
+    {
+        titulo: "Marianne Vos",
+        descricao: "É uma desportista neerlandesa que compete no ciclismo nas modalidades de estrada, pista (especialista nas provas de pontuação e scratch) e ciclocross. Pelo seu palmarés, bicampeã olímpica (Pequim 2008 e Londres 2012), três vezes campeã mundial em estrada, duas vezes campeã mundial em pista e sete vezes campeã mundial em ciclocross, é uma das ciclistas femininas mais laureadas da história e a única em coroar-se em três especialidades diferentes. Participou em três Jogos Olímpicos de Verão, entre os anos 2008 e 2016, obtendo ao todo duas medalhas de ouro, a primeira em Pequim 2008, na carreira por pontos (ciclismo em pista), e a segunda em Londres 2012, na prova de rota feminina.",
+        link: "https://pt.wikipedia.org/wiki/Marianne_Vos",
+        tags: ""
+    },
+    {
+        titulo: "",
+        descricao: "",
+        link: "",
+        tags: ""
+    },
+];
